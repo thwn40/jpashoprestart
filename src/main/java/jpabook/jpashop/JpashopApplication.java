f0f640abdd3eed2338c8da2f1a09cadefbee8ac0
thwn40 <62760166+thwn40@users.noreply.github.com>
2022-05-06 23:54:19 +0900

    20220506 jpa 활용2

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -1,8 +1,10 @@
 package jpabook.jpashop;
 
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 import jpabook.jpashop.domain.Member;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
@@ -13,7 +15,12 @@ public class JpashopApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JpashopApplication.class, args);
+
 	}
 
+	@Bean
+	Hibernate5Module hibernate5Module() {
+		return new Hibernate5Module();
 
+	}
 }
